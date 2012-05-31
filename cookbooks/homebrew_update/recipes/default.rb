176d088cd7cdac98ddb7ee02782daadf7e71876a
Steve Hoeksema <steve@seven.net.nz>
2012-05-31 16:23:06 +1200

    tap brews

--- cookbooks/homebrew_update/recipes/default.rb
+++ cookbooks/homebrew_update/recipes/default.rb
@@ -1,3 +1,9 @@
 execute "update homebrew from github" do
   command "/usr/local/bin/brew update || true"
 end
+
+execute "tap" do
+  command "/usr/local/bin/brew tap adamv/alt || true"
+  command "/usr/local/bin/brew tap josegonzalez/php || true"
+  command "/usr/local/bin/brew tap steveh/custom || true"
+end
