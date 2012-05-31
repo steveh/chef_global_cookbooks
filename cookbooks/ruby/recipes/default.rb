b32ff8930b89fe97d9c454c23b1582f67308dd84
Steve Hoeksema <steve@seven.net.nz>
2012-05-31 15:43:46 +1200

    extra ruby packages

--- cookbooks/ruby/recipes/default.rb
+++ cookbooks/ruby/recipes/default.rb
@@ -2,4 +2,11 @@ if platform?("mac_os_x")
   # built in
 else
   package "ruby1.9.3"
+  package "ruby1.9.1-dev"
+  package "libxml2-dev"
+  package "libxslt1-dev"
+  package "libcurl4-openssl-dev"
+  package "libyaml-dev"
 end
+
+gem_package "bundler"
